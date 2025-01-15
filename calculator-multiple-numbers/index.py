64d1819ec68f88c67051d663933c6d74742a19bf
fkrmubaroq <fkrmubaroq@gmail.com>
2025-01-15 11:49:28 +0700

    completed create code calculator multiple numbers

---
+++ calculator-multiple-numbers/index.py
@@ -0,0 +1,49 @@
+import art
+print(art.logo)
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+should_accumulate = True
+result = 0
+num1 = float(input("What is the first number?: "))
+tasks = []
+index = 0
+while should_accumulate:
+    is_first_index = index == 0
+    num2 = float(input("What is the next number?: "))
+    operation_symbol = input("Pick an operation: ")
+    if operation_symbol not in operations:
+        print("unknown operation!")
+        continue
+    if index == 0:
+        result = operations[operation_symbol](num1, num2)
+    else:
+        result = operations[operation_symbol](result, num2)
+
+    print(f"Result is :{result}")
+    is_continue = input("Continue ? Y/N : ").lower()
+    if is_continue == "n":
+        print("Goodbye~")
+        should_accumulate = False
+
+    index += 1
+
+
